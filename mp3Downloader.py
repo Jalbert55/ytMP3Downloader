bac9495103f188706ac3c6709c2d4829e56947fb
Jose Alberto Mendoza Chavez <jmendozac1200@alumno.ipn.mx>
2024-11-05 23:31:16 -0600

    Cambiando UI

--- mp3Downloader.py
+++ mp3Downloader.py
@@ -6,6 +6,8 @@ from pytube import Playlist
 import os
 import threading
 import queue
+import subprocess
+from PIL import Image, ImageTk
 
 # Cola para comunicar el progreso
 progress_queue = queue.Queue()
@@ -76,6 +78,7 @@ def iniciar_descarga():
     # Resetear el evento de cancelación
     cancel_event.clear()
     output_text.delete(1.0, tk.END)  # Limpiar la salida de texto
+    output_text.insert(tk.END, "INICIANDO DESCARGA\n")
 
     # Iniciar la descarga en un hilo separado
     if "playlist" or "list" in url:
@@ -91,26 +94,68 @@ def cancelar_descarga():
     output_text.insert(tk.END, "Descarga cancelada por el usuario.\n")
     output_text.see(tk.END)  # Desplaza el texto hacia el final
 
-# Función para cerrar la ventana y detener hilos
-def on_closing():
-    cancel_event.set()  # Activa el evento de cancelación para todos los hilos
-    root.destroy()  # Cierra la ventana
+def abrir_carpeta():
+    """Abre la carpeta de descargas en el explorador de archivos."""
+    output_path = entry_path.get()
+    if os.path.isdir(output_path):
+        try:
+            if os.name == 'nt':  # Windows
+                os.startfile(output_path)
+            elif os.name == 'posix':  # macOS o Linux
+                subprocess.Popen(['open', output_path] if sys.platform == 'darwin' else ['xdg-open', output_path])
+        except Exception as e:
+            output_text.insert(tk.END, f"Error al abrir la carpeta: {e}\n")
+            output_text.see(tk.END)
+    else:
+        output_text.insert(tk.END, "La ruta de salida no es válida.\n")
+        output_text.see(tk.END)
+
+# Función para habilitar/deshabilitar el botón de descarga
+def verificar_campos():
+    if entry_url.get().strip() and entry_path.get().strip():
+        button_download.config(state=tk.NORMAL)
+    else:
+        button_download.config(state=tk.DISABLED)
 
 # Crear la ventana principal
 root = tk.Tk()
 root.title("Descargador de YouTube")
-root.geometry("600x400")  # Tamaño de la ventana
+
+# Centrar la ventana en la pantalla
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+window_width = 700
+window_height = 400
+x = (screen_width/2) - (window_width/2)
+y = (screen_height/2) - (window_height/2)
+root.geometry(f"{window_width}x{window_height}+{int(x)}+{int(y)}")
+
+
+# Hacer que la ventana se ajuste al contenido
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
+
+# Configurar el fondo y color de fuente
+root.configure(bg="#000000")  # Fondo rojo
+label_color = "white"  # Color de fuente de los labels
 
 # Crear widgets de la interfaz
-label_url = tk.Label(root, text="URL del video o playlist:")
-label_url.pack(pady=10)
+label_url = tk.Label(root, text="URL del video o playlist:", bg="#000000", fg=label_color, font=('Times 20'))
+label_url.grid(row=0, column=0, padx=10, pady=10, sticky='w')
 entry_url = tk.Entry(root, width=50)
-entry_url.pack(pady=10)
+entry_url.grid(row=0, column=1, columnspan=2, padx=10, pady=10)
+entry_url.bind("<KeyRelease>", lambda event: verificar_campos())  # Llama a verificar_campos cuando se escribe en URL
 
-label_path = tk.Label(root, text="Ruta de salida:")
-label_path.pack(pady=10)
-entry_path = tk.Entry(root, width=50)
-entry_path.pack(pady=10)
+label_path = tk.Label(root, text="Ruta de salida:", bg="#000000", fg=label_color, font=('Times 20'))
+label_path.grid(row=1, column=0, padx=10, pady=10, sticky='w')
+entry_path = tk.Entry(root, width=35)
+entry_path.grid(row=1, column=1, padx=10, pady=10, sticky='w')
+entry_path.bind("<KeyRelease>", lambda event: verificar_campos())  # Llama a verificar_campos cuando se escribe en Ruta
+
+# Cargar el icono
+icon = Image.open("icono.png")
+photo = ImageTk.PhotoImage(icon)
+root.iconphoto(False, photo)
 
 # Botón para seleccionar la ruta de salida
 def seleccionar_ruta():
@@ -118,23 +163,44 @@ def seleccionar_ruta():
     if ruta:
         entry_path.delete(0, tk.END)
         entry_path.insert(0, ruta)
+    verificar_campos()  # Verificar los campos después de seleccionar la ruta
 
-button_select_path = tk.Button(root, text="Seleccionar carpeta", command=seleccionar_ruta)
-button_select_path.pack(pady=10)
+# Función para cargar y redimensionar imágenes
+def cargar_imagen(ruta, ancho, alto):
+    imagen = Image.open(ruta)  # Abrir la imagen
+    imagen_redimensionada = imagen.resize((ancho, alto), resample=Image.LANCZOS)  # Redimensionar la imagen
+    return ImageTk.PhotoImage(imagen_redimensionada)  # Convertir a PhotoImage
 
-# Botón para iniciar la descarga
-button_download = tk.Button(root, text="Iniciar descarga", command=iniciar_descarga)
-button_download.pack(pady=10)
+# Cargar imágenes y redimensionarlas a 50x50 píxeles
+img_select_path = cargar_imagen("buscar.png", 50, 50)
+img_open_folder = cargar_imagen("carpeta.png", 50, 50)
+img_cancel = cargar_imagen("cancelar.png", 50, 50)
+img_download = cargar_imagen("descargar.png", 50, 50)
 
-# Botón para cancelar la descarga
-button_cancel = tk.Button(root, text="Cancelar descarga", command=cancelar_descarga)
-button_cancel.pack(pady=10)
+# Botón para seleccionar la ruta de salida
+button_select_path = tk.Button(root, image=img_select_path, command=seleccionar_ruta)
+button_select_path.grid(row=1, column=2, padx=5, pady=10, sticky='w')
+
+# Botón para abrir la carpeta de descargas
+button_open_folder = tk.Button(root, image=img_open_folder, command=abrir_carpeta)
+button_open_folder.grid(row=1, column=3, padx=5, pady=10, sticky='w')
+
+# Botón para iniciar la descarga y cancelar la descarga en la misma línea
+button_download = tk.Button(root, image=img_download, command=iniciar_descarga, state=tk.DISABLED)
+button_download.grid(row=2, column=1, padx=10, pady=10, sticky='e')
+
+button_cancel = tk.Button(root, image=img_cancel, command=cancelar_descarga)
+button_cancel.grid(row=2, column=2, padx=10, pady=10, sticky='w')
 
 # Recuadro de texto para mostrar la salida de la consola
 output_text = scrolledtext.ScrolledText(root, width=70, height=10)
-output_text.pack(pady=10)
+output_text.grid(row=3, column=0, columnspan=4, padx=10, pady=10)
 
 # Configurar el cierre de la ventana para cancelar la descarga
+def on_closing():
+    cancelar_descarga()
+    root.destroy()
+
 root.protocol("WM_DELETE_WINDOW", on_closing)
 
 # Iniciar el bucle principal de la interfaz
